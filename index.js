ea8d0ef4af9477d5418a9703dc2f7c61e019cd9c
rashidsarkar <rashidrock558@gmail.com>
2023-11-05 09:28:45 +0600

    Configure CORS middleware to allow requests from http://localhost:5173 with credentials

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
