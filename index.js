4b8d0f88cd5f7a9ef1639b077edb47d333c42b0f
rashidsarkar <rashidrock558@gmail.com>
2023-11-06 21:21:46 +0600

    Add CORS configuration for trusted origins

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fanciful-dango-1d1745.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
